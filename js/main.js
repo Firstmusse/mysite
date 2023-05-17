d849db39e268dea04f64784f3dd2362095e641b5
Aleksandr Stepanenko <mussefirst1@gmail.com>
2023-05-17 13:37:25 +0600

    add slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 const elem = document.querySelectorAll('.skills-content-item')
 const num = document.getElementById("number-skills")
 num.textContent = elem.length
+
+
+$(document).ready(function(){
+	$('.count-project').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
